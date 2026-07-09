f72bcfad546dcbb624909bfa87c9a8db110ed69c
Romi Setiawan <romi36825@gmail.com>
2026-07-09 07:52:31 +0700

    chore: remove unused static template files and assets

--- check-sessions.php
+++
@@ -1,5 +0,0 @@
-<?php
-$sessionsDir = 'storage/framework/sessions';
-echo "Directory: " . $sessionsDir . "\n";
-echo "Exists: " . (file_exists($sessionsDir) ? 'YES' : 'NO') . "\n";
-echo "Is Writable: " . (is_writable($sessionsDir) ? 'YES' : 'NO') . "\n";
